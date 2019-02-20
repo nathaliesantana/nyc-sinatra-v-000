dda94fcba07e316db237bbf93cdadcbd86924f2d
Nathalie Santana <nathalie.scruz@gmail.com>
2019-02-20 02:47:36 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,43 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    erb:'figures/index'
+  end
+
+  get '/figures/new' do
+    erb:'figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb:'/figures/show'
+  end
+
+ post '/figures' do
+    @figure = Figure.create(params['figure'])
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  # get'/figures/:id/edit' do
+  #   @figure = Figure.find_by_id(params[:id])
+  #
+  #   erb:'figures/edit'
+  # end
+  #
+  # patch '/figures/:id' do
+  #   @figure = Figure.find_by_id(params[:id])
+  #   @figure.name = params[:name]
+  #   @figure.year_completed = params[:year_completed]
+  #   @figure.save
+  #
+  #   redirect"/figures/#{@figure.id}"
+  # end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,37 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+
+  get '/landmarks' do
+    erb:'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb:'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb:'/landmarks/show'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(name: params["landmark_name"], year_completed: params["landmark_year_completed"])
+
+    redirect"/landmarks/#{@landmark.id}"
+  end
+
+  get'/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+
+    erb:'landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.name = params[:name]
+    @landmark.year_completed = params[:year_completed]
+    @landmark.save
+
+    redirect"/landmarks/#{@landmark.id}"
+  end
+
 end
